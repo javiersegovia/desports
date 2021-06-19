e90f54340fabd71c4d11b236ddb69e507e3014be
Javier Segovia <javiersegovia@pop-os.localdomain>
2021-06-18 23:00:18 -0400

    base animations

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,168 +1,172 @@
-import { gsap } from 'gsap'
 import { ScrollTrigger } from 'gsap/dist/ScrollTrigger'
 import { FullScreen } from '@components/UI/FullScreen'
 import { Features, Landing, Roadmap, Tokenomics } from '@views/home'
 import { useEffect, useRef } from 'react'
-
-let flag = false
-
-export const scrollToSectionY = (i: number) => {
-  console.log({ flag })
-  if (flag) return
-
-  gsap.to(window, {
-    scrollTo: { y: i * innerHeight, autoKill: false },
-    duration: 0.65,
-    onStart: () => {
-      flag = true
-    },
-    onComplete: () => {
-      flag = false
-    },
-  })
-
-  // if (anim) {
-  //   anim.restart()
-  // }
-}
-
-// export const scrollToSectionX = (i: number) => {
-//   gsap.to(window, {
-//     scrollTo: { x: i * innerWidth, autoKill: false },
-//     duration: 1,
-//   })
-
-//   // if (anim) {
-//   //   anim.restart()
-//   // }
-// }
+import { useAnimations } from '../views/home/useAnimations'
+import tw from 'twin.macro'
 
 export default function Home() {
-  const landingRef = useRef<HTMLDivElement>(null)
-  const featuresRef = useRef<HTMLDivElement>(null)
-  const tokenomicsRef = useRef<HTMLDivElement>(null)
-  const roadmapRef = useRef<HTMLDivElement>(null)
-
-  const r1Ref = useRef<HTMLDivElement>(null)
-  const r2Ref = useRef<HTMLDivElement>(null)
-  const r3Ref = useRef<HTMLDivElement>(null)
-
-  const hWrapperRef = useRef<HTMLDivElement>(null)
-  const hContainerRef = useRef<HTMLDivElement>(null)
-
-  useEffect(() => {
-    const verticalEls = [
-      landingRef.current,
-      featuresRef.current,
-      tokenomicsRef.current,
-      roadmapRef.current,
-      hContainerRef.current,
-    ]
-
-    verticalEls.forEach((section, i) => {
-      ScrollTrigger.create({
-        trigger: section,
-        invalidateOnRefresh: true,
-        onEnter: () => scrollToSectionY(i),
-      })
-
-      ScrollTrigger.create({
-        trigger: section,
-        start: 'bottom bottom',
-        invalidateOnRefresh: true,
-        onEnterBack: () => scrollToSectionY(i),
-      })
-    })
-
-    const horizontalEls = [r1Ref.current, r2Ref.current, r3Ref.current]
-    const hContainer = hContainerRef.current as HTMLDivElement
-
-    const els = gsap.utils.toArray(horizontalEls)
-
-    gsap.to(els, {
-      xPercent: -100 * (els.length - 1),
-      duration: 0.2,
-      ease: 'none',
-      scrollTrigger: {
-        trigger: hContainer,
-        pin: true,
-        start: 'top',
-        markers: true,
-        scrub: 1,
-        end: () => '+=' + hContainer.offsetWidth / (els.length - 1) / 3.5,
-      },
-    })
-
-    // gsap.to(hContainer, {
-    //   x: () =>
-    //     -(hContainer.scrollWidth - document.documentElement.clientWidth) + 'px',
-    //   ease: 'none',
-    //   scrollTrigger: {
-    //     trigger: hContainer,
-    //     invalidateOnRefresh: true,
-    //     pin: true,
-    //     scrub: 1,
-    //     end: () => '+=' + hContainer.offsetWidth,
-    //   },
-    // })
-  }, [])
+  const {
+    isScrolling,
+    parentRef,
+    containerRef,
+    activeNav,
+    landingRef,
+    featuresRef,
+    tokenomicsRef,
+    roadmapContainerRef,
+    navCheckpointRef,
+    navRef,
+    s0Ref,
+    s1Ref,
+    s2Ref,
+    s3Ref,
+  } = useAnimations()
 
   return (
     <>
-      <FullScreen
-        sectionRef={landingRef}
-        tw="bg-red-500 flex items-center justify-center sticky"
-      >
-        <Landing />
-      </FullScreen>
+      {/* <FullScreen tw="bg-indigo-200 w-full h-full">1</FullScreen> */}
 
-      <FullScreen
-        sectionRef={featuresRef}
-        tw="bg-green-500 flex items-center justify-center sticky"
-      >
-        <Features />
-      </FullScreen>
+      <div tw="absolute overflow-hidden w-full h-full z-50">
+        <div ref={containerRef} tw="w-full h-full">
+          <FullScreen
+            sectionRef={landingRef}
+            tw="bg-red-500 flex items-center justify-center"
+          >
+            <Landing />
+          </FullScreen>
+          <FullScreen
+            sectionRef={featuresRef}
+            tw="bg-green-500 flex items-center justify-center"
+          >
+            <Features />
+          </FullScreen>
+          <FullScreen
+            sectionRef={tokenomicsRef}
+            tw="bg-yellow-300 flex items-center justify-center"
+          >
+            <Tokenomics />
+          </FullScreen>
 
-      <FullScreen
-        sectionRef={tokenomicsRef}
-        tw="bg-yellow-300 flex items-center justify-center sticky"
-      >
-        <Tokenomics />
-      </FullScreen>
+          <FullScreen
+            tw="width[400%] flex flex-row flex-nowrap sticky"
+            sectionRef={roadmapContainerRef}
+          >
+            <FullScreen
+              sectionRef={s0Ref}
+              tw="bg-purple-300 flex items-center justify-center"
+            >
+              Stage 0
+            </FullScreen>
+            <FullScreen
+              sectionRef={s1Ref}
+              tw="bg-pink-400 flex items-center justify-center"
+            >
+              STAGE1
+            </FullScreen>
+            <FullScreen
+              sectionRef={s2Ref}
+              tw="bg-green-300 min-width[100vw] w-screen flex items-center justify-center"
+              data-tag="stage2"
+            >
+              <div css={[activeNav === 'stage2' && tw`text-4xl font-bold`]}>
+                {activeNav}
+                Stage 2
+              </div>
+            </FullScreen>
+            <FullScreen
+              sectionRef={s3Ref}
+              tw="bg-purple-300 min-width[100vw] w-screen flex items-center justify-center"
+              data-tag="stage3"
+            >
+              <div css={[activeNav === 'stage3' && tw`text-4xl font-bold`]}>
+                {activeNav}
+                Stage 3
+              </div>
+            </FullScreen>
+          </FullScreen>
+        </div>
+      </div>
 
-      <FullScreen
+      {/* <FullScreen
+        sectionRef={parentRef}
+        tw="relative bg-black w-full h-full z-30"
+      >
+        <div>dsadads</div>
+      </FullScreen> */}
+
+      {/* <FullScreen
         isHorizontal
         sectionRef={roadmapRef}
         tw="bg-blue-500 flex items-center justify-center sticky"
       >
         roadmap
-      </FullScreen>
+      </FullScreen> */}
+
+      {/* <FullScreen tw="bg-blue-200 w-full h-full" /> */}
+      {/* <FullScreen tw="bg-red-200 w-full h-full" /> */}
+
+      {/* <div
+        tw="fixed bottom-4 left-1/2 transform -translate-x-1/2 translate-y-40 mx-auto z-50"
+        ref={navRef}
+      >
+        <div tw="flex items-center justify-center space-x-4">
+          <div tw="bg-gray-400 font-medium">Stage 1</div>
+          <div tw="bg-gray-400 font-medium">Stage 2</div>
+          <div tw="bg-gray-400 font-medium">Stage 3</div>
+          <div tw="bg-gray-400 font-medium">Stage 4</div>
+        </div>
+      </div>
+
 
       <FullScreen
         tw="width[400%] flex flex-row flex-nowrap sticky"
-        sectionRef={hContainerRef}
+        sectionRef={roadmapContainerRef}
       >
         <>
+          <div ref={navCheckpointRef} tw="overflow-hidden" />
           <FullScreen
-            sectionRef={r1Ref}
+            sectionRef={s0Ref}
+            tw="bg-blue-500 min-width[100vw] w-screen flex items-center justify-center relative"
+            data-tag="roadmap"
+          >
+            <div css={[activeNav === 'roadmap' && tw`text-4xl font-bold`]}>
+              RoadMap
+              {activeNav}
+            </div>
+          </FullScreen>
+          <FullScreen
+            sectionRef={s1Ref}
             tw="bg-red-300 min-width[100vw] w-screen flex items-center justify-center"
+            data-tag="stage1"
           >
-            dasdasdas
+            <div css={[activeNav === 'stage1' && tw`text-4xl font-bold`]}>
+              {activeNav}
+              Stage 1
+            </div>
           </FullScreen>
           <FullScreen
-            sectionRef={r2Ref}
+            sectionRef={s2Ref}
             tw="bg-green-300 min-width[100vw] w-screen flex items-center justify-center"
+            data-tag="stage2"
           >
-            lalo
+            <div css={[activeNav === 'stage2' && tw`text-4xl font-bold`]}>
+              {activeNav}
+              Stage 2
+            </div>
           </FullScreen>
           <FullScreen
-            sectionRef={r3Ref}
+            sectionRef={s3Ref}
             tw="bg-purple-300 min-width[100vw] w-screen flex items-center justify-center"
+            data-tag="stage3"
           >
-            threee yes!!!
+            <div css={[activeNav === 'stage3' && tw`text-4xl font-bold`]}>
+              {activeNav}
+              Stage 3
+            </div>
           </FullScreen>
         </>
-      </FullScreen>
+      </FullScreen> */}
     </>
   )
 }
---
+++ src/views/home/Navigation.tsx
@@ -0,0 +1,11 @@
+export const Navigation = () => {
+  return (
+    <div tw="flex items-center justify-center space-x-4">
+      <div tw="bg-gray-400 font-medium">Stage 1</div>
+      <div tw="bg-gray-400 font-medium">Stage 2</div>
+      <div tw="bg-gray-400 font-medium">Stage 3</div>
+      <div tw="bg-gray-400 font-medium">Stage 4</div>
+    </div>
+  )
+}
+
