ee6881f6e113145abf076459d39533ccf476c21a
Javier Segovia <jsegoviadev@gmail.com>
2021-06-20 04:04:50 -0400

    Clean up the animations

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,17 +4,20 @@ import { useAnimations } from '@views/home/useAnimations'
 
 export default function Home() {
   const {
+    navigate,
     wrapperRef,
     containerRef,
     landingRef,
     featuresRef,
     tokenomicsRef,
+    navRef,
     roadmapContainerRef,
     s0Ref,
     s1Ref,
     s12Ref,
     s2Ref,
     s3Ref,
+    s4Ref,
   } = useAnimations()
 
   return (
@@ -42,7 +45,7 @@ export default function Home() {
 
           <FullScreen
             data-name="hContainer"
-            tw="width[500%] flex flex-row flex-nowrap sticky"
+            tw="width[500%] flex flex-row flex-nowrap relative"
             sectionRef={roadmapContainerRef}
           >
             <FullScreen
@@ -87,87 +90,52 @@ export default function Home() {
             >
               Stage 3
             </FullScreen>
+            <FullScreen
+              data-name="s4"
+              sectionRef={s4Ref}
+              tw="bg-indigo-400 min-width[100vw] w-screen flex items-center justify-center"
+              data-tag="stage4"
+            >
+              Stage 4
+            </FullScreen>
           </FullScreen>
         </div>
-      </div>
-
-      {/* <FullScreen
-        sectionRef={parentRef}
-        tw="relative bg-black w-full h-full z-30"
-      >
-        <div>dsadads</div>
-      </FullScreen> */}
-
-      {/* <FullScreen
-        isHorizontal
-        sectionRef={roadmapRef}
-        tw="bg-blue-500 flex items-center justify-center sticky"
-      >
-        roadmap
-      </FullScreen> */}
-
-      {/* <FullScreen tw="bg-blue-200 w-full h-full" /> */}
-      {/* <FullScreen tw="bg-red-200 w-full h-full" /> */}
-
-      {/* <div
-        tw="fixed bottom-4 left-1/2 transform -translate-x-1/2 translate-y-40 mx-auto z-50"
-        ref={navRef}
-      >
-        <div tw="flex items-center justify-center space-x-4">
-          <div tw="bg-gray-400 font-medium">Stage 1</div>
-          <div tw="bg-gray-400 font-medium">Stage 2</div>
-          <div tw="bg-gray-400 font-medium">Stage 3</div>
-          <div tw="bg-gray-400 font-medium">Stage 4</div>
-        </div>
-      </div>
-      <FullScreen
-        tw="width[400%] flex flex-row flex-nowrap sticky"
-        sectionRef={roadmapContainerRef}
-      >
-        <>
-          <div ref={navCheckpointRef} tw="overflow-hidden" />
-          <FullScreen
-            sectionRef={s0Ref}
-            tw="bg-blue-500 min-width[100vw] w-screen flex items-center justify-center relative"
-            data-tag="roadmap"
-          >
-            <div css={[activeNav === 'roadmap' && tw`text-4xl font-bold`]}>
-              RoadMap
-              {activeNav}
-            </div>
-          </FullScreen>
-          <FullScreen
-            sectionRef={s1Ref}
-            tw="bg-red-300 min-width[100vw] w-screen flex items-center justify-center"
-            data-tag="stage1"
-          >
-            <div css={[activeNav === 'stage1' && tw`text-4xl font-bold`]}>
-              {activeNav}
+        <div
+          tw="absolute bottom-0 left-1/2 transform -translate-x-1/2 translate-y-10 mx-auto z-50"
+          ref={navRef}
+        >
+          <div tw="flex items-center justify-center space-x-4">
+            <button
+              type="button"
+              onClick={() => navigate(1)}
+              tw="bg-gray-400 font-medium"
+            >
               Stage 1
-            </div>
-          </FullScreen>
-          <FullScreen
-            sectionRef={s2Ref}
-            tw="bg-green-300 min-width[100vw] w-screen flex items-center justify-center"
-            data-tag="stage2"
-          >
-            <div css={[activeNav === 'stage2' && tw`text-4xl font-bold`]}>
-              {activeNav}
+            </button>
+            <button
+              type="button"
+              onClick={() => navigate(3)}
+              tw="bg-gray-400 font-medium"
+            >
               Stage 2
-            </div>
-          </FullScreen>
-          <FullScreen
-            sectionRef={s3Ref}
-            tw="bg-purple-300 min-width[100vw] w-screen flex items-center justify-center"
-            data-tag="stage3"
-          >
-            <div css={[activeNav === 'stage3' && tw`text-4xl font-bold`]}>
-              {activeNav}
+            </button>
+            <button
+              type="button"
+              onClick={() => navigate(4)}
+              tw="bg-gray-400 font-medium"
+            >
               Stage 3
-            </div>
-          </FullScreen>
-        </>
-      </FullScreen> */}
+            </button>
+            <button
+              type="button"
+              onClick={() => navigate(5)}
+              tw="bg-gray-400 font-medium"
+            >
+              Stage 4
+            </button>
+          </div>
+        </div>
+      </div>
     </>
   )
 }
--- src/views/home/useAnimations.tsx
+++ src/views/home/useAnimations.tsx
@@ -1,11 +1,29 @@
-import { useEffect, useRef } from 'react'
+import { useState, useEffect, useRef } from 'react'
 import { gsap } from 'gsap'
 import throttle from 'lodash.throttle'
+
+const transitionSpeed = 0.85
+const touchTransitionSpeed = 0.65
+
+let oldSlide = 0
+let activeSlide = 0
+let newSlide: number
+let insideRoadmap: boolean
+let lastY = 0
+let lastX = 0
+let offsetsY: number[] = []
+let offsetsX: number[] = []
+let lastEvent: WheelEvent | TouchEvent | null
 
 interface ScrollDirection {
   x: 'LEFT' | 'RIGHT' | null
   y: 'UP' | 'DOWN' | null
 }
+
+// todo: fix vertical resize
+
+const getSpeed = (event: WheelEvent | TouchEvent | null) =>
+  event instanceof WheelEvent ? touchTransitionSpeed : transitionSpeed
 
 export const useAnimations = () => {
   const wrapperRef = useRef<HTMLDivElement>(null)
@@ -13,133 +31,153 @@ export const useAnimations = () => {
   const landingRef = useRef<HTMLDivElement>(null)
   const featuresRef = useRef<HTMLDivElement>(null)
   const tokenomicsRef = useRef<HTMLDivElement>(null)
+
+  // Roadmap references
   const roadmapContainerRef = useRef<HTMLDivElement>(null)
+  const navRef = useRef<HTMLDivElement>(null)
   const s0Ref = useRef<HTMLDivElement>(null)
   const s1Ref = useRef<HTMLDivElement>(null)
   const s12Ref = useRef<HTMLDivElement>(null)
   const s2Ref = useRef<HTMLDivElement>(null)
   const s3Ref = useRef<HTMLDivElement>(null)
+  const s4Ref = useRef<HTMLDivElement>(null)
+
+  const ySections = useRef([
+    landingRef,
+    featuresRef,
+    tokenomicsRef,
+    roadmapContainerRef,
+  ])
+
+  const xSections = useRef([
+    roadmapContainerRef,
+    s0Ref,
+    s1Ref,
+    s12Ref,
+    s2Ref,
+    s3Ref,
+    s4Ref,
+  ])
+
+  const [info, setInfo] = useState<{
+    oldSlide: number
+    activeSlide: number
+    lastEvent: WheelEvent | TouchEvent | null
+    insideRoadmap: boolean
+  }>({
+    oldSlide: 0,
+    activeSlide: 0,
+    lastEvent: null,
+    insideRoadmap: false,
+  })
+
+  // todo: convert to useRef
+  const scrollTimeline = useRef(gsap.timeline())
+  const navTimeline = useRef(gsap.timeline())
+
+  type SlideAnimationFunc = (
+    e: WheelEvent | TouchEvent | number,
+    direction?: ScrollDirection
+  ) => void
+
+  const sections = [...ySections.current, ...xSections.current]
+  const lastIndexY = ySections.current.length - 2
+  const maxIndex = sections.length - 2
+
+  const slideAnimation = useRef<SlideAnimationFunc>(
+    throttle((e, direction) => {
+      if (gsap.isTweening(containerRef.current)) return
+
+      if (typeof e === 'number') {
+        newSlide = e
+        if (activeSlide === newSlide) return
+      } else {
+        lastEvent = e
+
+        const { x } = direction || {}
+        let { y } = direction || {}
+
+        // In wheel events we need to update the direction
+        if (lastEvent instanceof WheelEvent) y = e.deltaY > 0 ? 'DOWN' : 'UP'
+        if (activeSlide > lastIndexY) insideRoadmap = true
+        else insideRoadmap = false
+
+        const shouldAdvance = insideRoadmap && x ? x === 'LEFT' : y === 'DOWN'
+        newSlide = shouldAdvance ? activeSlide + 1 : activeSlide - 1
+
+        if (newSlide < 0) newSlide = 0
+        if (newSlide > maxIndex) newSlide = maxIndex
+
+        if (activeSlide === newSlide) return
+
+        // TRANSITION FROM VERTICAL SLIDER TO HORIZONTAL SLIDER
+        if (sections[newSlide] === sections[activeSlide]) {
+          newSlide = shouldAdvance ? newSlide + 1 : newSlide - 1
+        }
+      }
+
+      oldSlide = activeSlide
+      activeSlide = newSlide
+
+      if (activeSlide < ySections.current.length) {
+        lastY = offsetsY[activeSlide]
+      } else if (activeSlide <= maxIndex) {
+        const xIndex = activeSlide - (ySections.current.length - 1)
+        lastX = offsetsX[xIndex]
+      }
+
+      const speed = getSpeed(lastEvent)
+
+      scrollTimeline.current.to(containerRef.current, {
+        // Increase the scroll speed in touch devices
+        duration: speed,
+        x: lastX,
+        y: lastY,
+        ease: 'power2.inOut',
+      })
+
+      setInfo({
+        insideRoadmap,
+        activeSlide,
+        oldSlide,
+        lastEvent,
+      })
+    }, 200)
+  )
 
   useEffect(() => {
     const hContainer = roadmapContainerRef.current as HTMLDivElement
     const container = containerRef.current as HTMLDivElement
 
-    let oldSlide = 0
-    let activeSlide = 0
-    let newSlide: number
-    let insideXSection: boolean
-
-    let lastY = 0
-    let lastX = 0
-
-    const duration = 0.85
-    let offsetsY: number[] = []
-    let offsetsX: number[] = []
-
-    const ySections = [
-      landingRef.current,
-      featuresRef.current,
-      tokenomicsRef.current,
-      hContainer,
-    ] as HTMLDivElement[]
-
-    const xSections = [
-      hContainer,
-      s0Ref.current,
-      s1Ref.current,
-      s12Ref.current,
-      s2Ref.current,
-      s3Ref.current,
-    ] as HTMLDivElement[]
-
-    const sections = [...ySections, ...xSections]
-    const maxIndex = sections.length - 2
-
-    const tl = gsap.timeline()
-
-    const slideAnim = (
-      e: WheelEvent | TouchEvent,
-      direction?: ScrollDirection
-    ) => {
-      if (gsap.isTweening(container)) return
-
-      const { x } = direction || {}
-      let { y } = direction || {}
-
-      oldSlide = activeSlide
-
-      if (e instanceof WheelEvent) y = e.deltaY > 0 ? 'DOWN' : 'UP'
-
-      if (oldSlide >= ySections.length) insideXSection = true
-      else insideXSection = false
-
-      const shouldAdvance = insideXSection && x ? x === 'LEFT' : y === 'DOWN'
-      newSlide = shouldAdvance ? activeSlide + 1 : activeSlide - 1
-
-      if (newSlide < 0) newSlide = 0
-      if (newSlide > maxIndex) newSlide = maxIndex
-      if (oldSlide === newSlide) return
-
-      activeSlide = newSlide
-
-      // TRANSITION FROM VERTICAL SLIDER TO HORIZONTAL SLIDER
-      if (sections[activeSlide] === sections[oldSlide]) {
-        activeSlide = shouldAdvance ? activeSlide + 1 : activeSlide - 1
-      }
-
-      if (activeSlide < ySections.length) {
-        lastY = offsetsY[activeSlide]
-      } else if (activeSlide <= maxIndex) {
-        const xIndex = activeSlide - (ySections.length - 1)
-        lastX = offsetsX[xIndex]
-      }
-
-      tl.to(container, {
-        duration,
-        x: lastX,
-        y: lastY,
-        ease: 'power2.inOut',
-      })
-    }
-
     function newSize() {
       offsetsY = []
       offsetsX = []
 
-      // gsap.set(wrapperRef.current, {
-      //   height: window.innerHeight,
-      //   width: window.innerWidth,
-      // })
-      gsap.set(container, {
-        height: ySections.length * window.innerHeight,
-      })
-      // gsap.set(hContainer, {
-      //   width: (xSections.length - 1) * window.innerWidth,
-      // })
-
-      for (const ySection of ySections) {
-        offsetsY.push(-ySection.offsetTop)
+      for (const ySection of ySections.current) {
+        ySection.current && offsetsY.push(-ySection.current.offsetTop)
       }
 
-      xSections.forEach((xSection) => {
-        const elementOffsetLeft = xSection.offsetLeft
+      xSections.current.forEach((xSection) => {
+        if (!xSection.current) return
+
+        const elementOffsetLeft = xSection.current.offsetLeft
 
         if (
-          xSection.parentElement &&
-          !xSection.parentElement.isSameNode(hContainer) &&
-          !xSection.isSameNode(hContainer)
+          xSection.current.parentElement &&
+          !xSection.current.parentElement.isSameNode(hContainer) &&
+          !xSection.current.isSameNode(hContainer)
         ) {
           offsetsX.push(
-            -(xSection.parentElement.offsetLeft + elementOffsetLeft)
+            -(xSection.current.parentElement.offsetLeft + elementOffsetLeft)
           )
         } else {
           offsetsX.push(-elementOffsetLeft)
         }
       })
 
-      const xIndex = activeSlide - (ySections.length - 1)
+      const xIndex = activeSlide - (ySections.current.length - 1)
 
-      if (activeSlide < ySections.length) {
+      if (activeSlide < ySections.current.length) {
         lastY = offsetsY[activeSlide]
         lastX = 0
       } else {
@@ -152,24 +190,20 @@ export const useAnimations = () => {
       })
     }
 
-    window.addEventListener('wheel', slideAnim, { passive: false })
-    window.addEventListener('resize', newSize)
-    newSize()
-
     const throttledAnim = throttle(
-      (e: TouchEvent, direction: ScrollDirection) => slideAnim(e, direction),
+      (e: TouchEvent, direction: ScrollDirection) =>
+        slideAnimation.current(e, direction),
       200
     )
 
     let touchStartY: number
     let touchStartX: number
 
-    window.addEventListener('touchstart', (e: TouchEvent) => {
+    const onTouchStart = (e: TouchEvent) => {
       touchStartY = e.touches[0].clientY
       touchStartX = e.touches[0].clientX
-    })
-
-    window.addEventListener('touchmove', (e: TouchEvent) => {
+    }
+    const onTouchMove = (e: TouchEvent) => {
       const touchEndY = e.changedTouches[0].clientY
       const touchEndX = e.changedTouches[0].clientX
       const touchDistance = 20
@@ -193,24 +227,113 @@ export const useAnimations = () => {
           y,
         })
       }
-    })
+    }
+
+    window.addEventListener('wheel', slideAnimation.current, { passive: false })
+    window.addEventListener('resize', newSize)
+    window.addEventListener('touchstart', onTouchStart)
+    window.addEventListener('touchmove', onTouchMove)
+
+    newSize()
+
+    const scrollTL = scrollTimeline.current
+    const navTL = navTimeline.current
+    const sAnimation = slideAnimation.current
 
     return () => {
-      tl.kill()
+      //TODO: test the unmounting of this component
+      console.log('unmounted useAnimations')
+      scrollTL.kill()
+      navTL.kill()
+      window.removeEventListener('wheel', sAnimation)
+      window.removeEventListener('resize', newSize)
+      window.removeEventListener('touchstart', onTouchStart)
+      window.removeEventListener('touchmove', onTouchMove)
     }
   }, [])
 
+  const isVisible = useRef(false)
+  const isSmaller = useRef(false)
+
+  useEffect(() => {
+    const { activeSlide, oldSlide } = info
+
+    const speed = getSpeed(lastEvent)
+
+    const transitionIndex1 = lastIndexY + 1
+    const transitionIndex2 = lastIndexY + 2
+
+    // HIDE NAV (LEAVE ROADMAD INDEX)
+    if (activeSlide <= lastIndexY && isVisible.current) {
+      navTimeline.current.to(navRef.current, {
+        duration: speed * 0.5,
+        y: 100,
+        onStart: () => {
+          isVisible.current = false
+        },
+      })
+    }
+
+    // REVEAL NAV (ENTER ROADMAD INDEX)
+    if (
+      (activeSlide === transitionIndex1 || activeSlide === transitionIndex2) &&
+      oldSlide < activeSlide
+    ) {
+      navTimeline.current.to(navRef.current, {
+        delay: speed * 0.75,
+        duration: speed * 0.5,
+        y: -100,
+        onStart: () => {
+          isVisible.current = true
+        },
+      })
+    }
+
+    // RESTORE DEFAULTS (ENTER BACK ROADMAP INDEX)
+    if (
+      (activeSlide === transitionIndex1 || activeSlide === transitionIndex2) &&
+      isSmaller.current
+    ) {
+      navTimeline.current.to(navRef.current, {
+        scale: 1,
+        duration: speed,
+        onStart: () => {
+          isSmaller.current = false
+        },
+      })
+    }
+
+    // MAKE NAVIGATION SMALLER (ENTER STAGE 1)
+    if (activeSlide > transitionIndex2 && !isSmaller.current) {
+      navTimeline.current.to(navRef.current, {
+        scale: 2,
+        duration: speed,
+        onStart: () => {
+          isSmaller.current = true
+        },
+      })
+    }
+  }, [info, lastIndexY])
+
+  const navigate = (stageIndex: number) => {
+    slideAnimation.current(ySections.current.length + stageIndex)
+  }
+
   return {
+    navigate,
+    info,
     wrapperRef,
     containerRef,
     landingRef,
     featuresRef,
     tokenomicsRef,
     roadmapContainerRef,
+    navRef,
     s0Ref,
     s1Ref,
     s12Ref,
     s2Ref,
     s3Ref,
+    s4Ref,
   }
 }
